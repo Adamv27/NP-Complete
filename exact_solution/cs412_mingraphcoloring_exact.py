292e84f27b3214cb5a757bc6e23c865840f19f32
LPAshurst <LPAshurst@hotmail.com>
2024-11-18 09:58:37 -0500

    asdasd

---
+++ exact_solution/cs412_mingraphcoloring_exact.py
@@ -0,0 +1,22 @@
+"""
+    name:  Your name(s) here
+
+    Honor Code and Acknowledgments:
+
+            This work complies with the JMU Honor Code.
+
+           Comments here on your code and submission.
+"""
+
+import itertools
+
+
+# All modules for CS 412 must include a main method that allows it
+# to imported and invoked from other python scripts
+def main():
+    # your code here
+    pass
+
+
+if __name__ == "__main__":
+    main()
